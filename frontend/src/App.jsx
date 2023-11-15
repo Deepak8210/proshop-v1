832e9faf1b2eec15586720ea710b84e3eec240f7
Deepak8210 <deepakrajput058@gmail.com>
2023-11-15 20:46:36 +0530

    firstCommit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="w-full min-h-screen flex items-center justify-center">
+      Welcome to ProShop
+    </div>
+  );
+}
+
+export default App;
