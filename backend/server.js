26dc4e8d0ab84677bf8ca711d6c922a6e6d45c42
Deepak8210 <deepakrajput058@gmail.com>
2023-12-19 22:11:47 +0530

    dist delted

--- backend/server.js
+++ backend/server.js
@@ -46,13 +46,13 @@ const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/build")));
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 } else {
-  //routes
+  // routes
   app.get("/", (req, res) => {
     res.send("api is running");
   });
